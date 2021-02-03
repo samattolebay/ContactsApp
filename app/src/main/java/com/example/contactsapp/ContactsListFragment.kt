b07fb5dbf8c5f32b2175556d0728f6cd38fa9788
ordaboy <t.samat01@gmail.com>
2021-02-04 00:24:12 +0600

    added navigation between fragments

--- app/src/main/java/com/example/contactsapp/ContactsListFragment.kt
+++ app/src/main/java/com/example/contactsapp/ContactsListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.contactsapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ class ContactsListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        Log.d("Contacts", "Contacts Fragment created")
         return inflater.inflate(R.layout.fragment_contacts_list, container, false)
     }
 }
--- app/src/main/java/com/example/contactsapp/RecentsListFragment.kt
+++ app/src/main/java/com/example/contactsapp/RecentsListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.contactsapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +9,12 @@ import android.view.ViewGroup
 
 class RecentsListFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-           // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_recents_list, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Log.d("Recents", "Recents Fragment created")
+        return inflater.inflate(R.layout.fragment_recents_list, container, false)
     }
 }
